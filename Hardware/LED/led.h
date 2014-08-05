755f92b190d9f89f71a00dcaa579ed4764702e80
RdMaxes <yc.huang0522@gmail.com>
2014-08-05 22:32:27 +0800

    change On and Off logic

--- Hardware/LED/led.h
+++ Hardware/LED/led.h
@@ -8,15 +8,15 @@
 #define LED1_Port		GPIOA
 #define LED1_CLK		RCC_APB2Periph_GPIOA
 #define LED1_Pin		GPIO_Pin_11
-#define GREEN_ON();		GPIO_ResetBits( LED1_Port, LED1_Pin );  
-#define GREEN_OFF();	GPIO_SetBits( LED1_Port, LED1_Pin );
+#define GREEN_ON();		GPIO_SetBits( LED1_Port, LED1_Pin ); GPIO_SetBits 
+#define GREEN_OFF();	GPIO_ResetBits( LED1_Port, LED1_Pin );
 
 /*LED2 Pin Definition*/
 #define LED2_Port		GPIOA
 #define LED2_CLK		RCC_APB2Periph_GPIOA
 #define LED2_Pin		GPIO_Pin_12
-#define RED_ON();		GPIO_ResetBits( LED2_Port, LED2_Pin );  
-#define RED_OFF();		GPIO_SetBits( LED2_Port, LED2_Pin );
+#define RED_ON();		GPIO_SetBits( LED2_Port, LED2_Pin );  
+#define RED_OFF();		GPIO_ResetBits( LED2_Port, LED2_Pin );
 /*Function Prototype*/
 //LED Initial 
 void LED_Init(void);
